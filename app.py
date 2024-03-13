5b20091137c42781bc2fb5eca8e7a24347eda292
Silvio Lima <silviolima07@gmail.com>
2024-03-13 18:49:03 -0300

    teste

--- app.py
+++ app.py
@@ -4,21 +4,22 @@ import time
 
 st.title("TESTE2")
 # Input for the prompt
-prompt = st.chat_input("Ask Anything")
+prompt = st.text_input("Ask Anything")
 
 
 
 if prompt:
 
     # Display input prompt from user
-    #st.title("st.chat_message")
     with st.chat_message("user"):
         #st.title("first with")
         st.write(prompt)
 
     # Processing
-    #st.title("st.spiner")
     with st.spinner("Processing..."):
-       #st.title("ollama.list()")
-       st.write(ollama.list())
-       
+       try:
+         result = ollama.list()
+         st.write(result)
+         
+       except Exception as e:
+         st.error(f'Error: {e}')
