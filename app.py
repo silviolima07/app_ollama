08ea3c5d242722ea2005c4e3b6847e6af73243da
Silvio Lima <silviolima07@gmail.com>
2024-03-13 18:03:16 -0300

    teste

--- app.py
+++ app.py
@@ -6,12 +6,19 @@ st.title("TESTE2")
 # Input for the prompt
 prompt = st.chat_input("Ask Anything")
 
+
+
 if prompt:
 
     # Display input prompt from user
+    #st.title("st.chat_message")
     with st.chat_message("user"):
+        #st.title("first with")
         st.write(prompt)
 
     # Processing
+    #st.title("st.spiner")
     with st.spinner("Processing..."):
-       st.write(ollama.list())        
+       #st.title("ollama.list()")
+       st.write(ollama.list())
+       
